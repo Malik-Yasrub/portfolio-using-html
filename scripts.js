5d1a585ea616811d762118c0bc9fe76da0f76761
Malik Yasrub Bashir <37244052+Malik-Yasrub@users.noreply.github.com>
2018-04-22 23:41:50 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+(function() {
+var inputs = document.createElement('input');
+	
+Create the supports object
+var supports = {};	
+supports.autofocus   = 'autofocus' in inputs;
+supports.required    = 'required' in inputs;
+supports.placeholder = 'placeholder' in inputs;
+
+if(!supports.autofocus) {
+		
+}
+if(!supports.required) {
+		
+}
+if(!supports.placeholder) {
+		
+}
+var send = document.getElementById('contact-submit');
+if(send) {
+send.onclick = function () {
+this.innerHTML = '...Sending';
+}
+}
+
+})();
